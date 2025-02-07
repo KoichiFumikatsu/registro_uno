fe7e1fe1d3da2c9da90ad3313d37d181febed769
Koichi <fumikatsu.koichi@gmail.com>
2025-02-07 02:23:28 -0500

    30% Empleados

--- main/app/empleados.php
+++ main/app/empleados.php
@@ -1,22 +1,29 @@
-<?php 
-	echo "Empleados";
-?>
+<div class="container">
+<!-- Card with an image on top -->
+	<div class="col">
+		<div class="card">	
+			<div class="card-body">
+				<div class="card-title">Empleados</div>
+				<div class="card-text">	
+					Grid de Cards con fotos, cuando selecciona uno, el grid se divide para mostrar los detalles en la siguiente mitad y abre una card que llama a otro php y dentro de ese dejar las opciones de editar. 
+				</div><br>
+				<div class="row">	
+					<?php for ($i=0; $i < 12; $i++) {  /*Aca se reemplaza por un foreach que itere la base de datos de empleados*/ ?>
+					<div class="col-md-3">
+						<div class="card">
+							<img src="assets/img/card.jpg" class="card-img-top" alt="...">
+							<div class="card-body ">
+								<h5 class="card-title">Nombre Apellido</h5>
+								<p class="card-text">Algunos de sus datos Base</p>
+								<button>un boton</button>
+							</div>
+						</div>
+					</div>
+					<?php } ?>
+				</div>				
+			</div>
+		</div>		
+	</div>
 
-Tenemos Claro que se debe de ver si o si 
-lista de empleados. 
-y edicion de empleado *donde solo cambia el status* 
-tal vez cambio de cargo? 
-puede ser tambien el cambio de firma. 
-
-ya se, mostremos Cards
-que haya un Buscador igualmente y filtros
-y que si selecciono uno, puede ser que despliegue en la sigueinte mitad o que muestre un modal. 
-
-
-Actualizacion. 
-
-Grid de CArds con fotos y demas 
-y cuando selecciona uno, el grid se divide para mostrar los detalles en la siguiente mitad
-y abre una card que llama a otro php y dentro de ese dejar las opciones de editar. 
-
-
+<!-- End Card with an image on top -->
+</div>
