ecb56473319dbac0a9c4352dd5a6be980f47ac0d
KoichiF <fumikatsu.koichi@gmail.com>
2025-02-07 19:45:07 -0500

    Changes 7_feb-2025

--- main/app/empleados.php
+++ main/app/empleados.php
@@ -15,7 +15,30 @@
 							<div class="card-body ">
 								<h5 class="card-title">Nombre Apellido</h5>
 								<p class="card-text">Algunos de sus datos Base</p>
-								<button>un boton</button>
+								<!-- Large Modal -->
+								<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#largeModal">
+								Detalles 
+								</button>
+
+								<div class="modal fade" id="largeModal" tabindex="-1">
+									<div class="modal-dialog modal-dialog-centered  modal-lg">
+										<div class="modal-content">
+											<div class="modal-header">
+												<h5 class="modal-title">Large Modal</h5>
+												<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+											</div>
+											<div class="modal-body">
+												<?php include('profile.php'); ?>
+
+												Non omnis incidunt qui sed occaecati magni asperiores est mollitia. Soluta at et reprehenderit. Placeat autem numquam et fuga numquam. Tempora in facere consequatur sit dolor ipsum. Consequatur nemo amet incidunt est facilis. Dolorem neque recusandae quo sit molestias sint dignissimos.
+											</div>
+											<div class="modal-footer">
+												<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+													<button type="submit" class="btn btn-primary">Guardar</button>
+											</div>
+										</div>
+									</div>
+								</div><!-- End Large Modal-->
 							</div>
 						</div>
 					</div>
